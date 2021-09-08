e8ca345ed94800fc3540bc18d883252be1f8bed4
Meerwaldten <mikkell_m_j@hotmail.com>
2021-09-08 10:36:07 +0200

    Test 3

--- src/calculator.java
+++ src/calculator.java
@@ -6,5 +6,18 @@ public class calculator {
         System.out.println("Rasmus er en bandit");
         System.out.println("bandit");
         System.out.println("hej theis");
+
+
+
+
+
     }
+
+    public static void plus(int a, int b) {
+        int result = a+b;
+        System.out.println(result);
+
+
+    }
+
 }
